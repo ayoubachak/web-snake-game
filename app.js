6e5ed7ce141e9ce6adce3cab5e97676c78ac76d8
Ayoub Achak <ayoub.achak01@gmail.com>
2022-11-15 13:09:32 +0100

    click the shift to speed

--- app.js
+++ app.js
@@ -9,17 +9,36 @@ context = canvas.getContext('2d');
 document.addEventListener("keydown",direction);
 function direction(event){
     let key = event.keyCode;
-    if( (key == 37 || key == 65) && direct != "RIGHT"){
-        direct = "LEFT";
-    }else if( ( key == 38 || key == 87) && direct != "DOWN"){
-        direct = "UP";
-    }else if( (key == 39 || key == 68) && direct != "LEFT"){
-        direct = "RIGHT";
-    }else if( ( key == 40 || key == 83) && direct != "UP"){
-        direct = "DOWN";
+
+    // check if the speed key is pressed
+    if (key == 16) {
+        if(game.gameTick >= 50){
+            game.gameTick-=5
+        }
+    }else{ // check the direction 
+        if( (key == 37 || key == 65) && direct != "RIGHT"){
+            direct = "LEFT";
+        }else if( ( key == 38 || key == 87) && direct != "DOWN"){
+            direct = "UP";
+        }else if( (key == 39 || key == 68) && direct != "LEFT"){
+            direct = "RIGHT";
+        }else if( ( key == 40 || key == 83) && direct != "UP"){
+            direct = "DOWN";
+        }
+        game.snake.direct = direct;
     }
-    game.snake.direct = direct;
+
+
+
+
 }
+document.addEventListener("keyup",function(event) {
+    let key = event.keyCode;
+    if (key == 16) {
+        game.gameTick = defaultTick;
+        console.log("cancel")
+    }
+})
 document.getElementById("start-button").addEventListener("click", function(){
     if(!game.gameStarted ){
         let level = document.getElementById("level-select").value
@@ -74,6 +93,7 @@ document.getElementById("continue-button").addEventListener("click", function(){
 document.getElementById("reset-button").addEventListener("click", function(){
     game.reset();
 })
+
 // document.getElementById("start-button").addEventListener("click", function(){
 //     // game.reset();
 // })
@@ -115,6 +135,7 @@ var rectangles1 = [
 var highScore = 0;
 var blockSize = 10;
 var gameTick = 100;
+var defaultTick = 100;
 var direct = "";
 var game = new Game({
     ctx:context, 
